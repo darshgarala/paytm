de3745ba7c86d71e2c91f396425301cdb8da7a1f
darshgarala <darshgarala01@gmail.com>
2024-06-22 15:15:56 +0530

    changis_id

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3001;
 const mainRouter = require("./routes/index.js");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
 
 app.use("/api/v1", mainRouter);
 
-app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
